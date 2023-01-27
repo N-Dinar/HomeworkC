d63799290f9cd064b60c51a45da39aba77c99960
Dinar <antaresdin@yandex.ru>
2023-01-27 15:01:14 +0500

    Task 8 code

---
+++ Task_8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+while (i < n)
+{
+    i += 2;
+    Console.Write("{0} ",i);   
+}
+
