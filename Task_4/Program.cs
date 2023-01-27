ba2c8bdf173f3c5c2a7c3b8818e0828e60fb5dbc
Dinar <antaresdin@yandex.ru>
2023-01-27 14:54:48 +0500

    Task 4 code

---
+++ Task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-ое число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int  max = a;
+    if(b > max) max = b;
+    if(c > max) max = c;
+Console.WriteLine($"Максимальное число: {max}");
