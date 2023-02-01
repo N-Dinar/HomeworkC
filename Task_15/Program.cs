f01771f0827bec7d00998a352870c850f7397d37
Dinar <antaresdin@yandex.ru>
2023-02-01 11:23:17 +0500

    Second homework task 15

---
+++ Task_15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("1-Понедельник, 2-Вторник, 3-Среда, 4-Четверг, 5-Пятница, 6-Суббота, 7-Воскресенье ");
+Console.Write("Введите число соответствующий дню недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n > 0 && n < 8)
+{
+    if (n < 6) Console.Write("Рабочий день!");
+    if (n > 5) Console.Write("Выходной!");
+}
+else 
+Console.Write("Неправильный ввод!");
