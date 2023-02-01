82e58a773991e99c8ab43a2b371020e46c57a33e
Dinar <antaresdin@yandex.ru>
2023-02-01 11:09:00 +0500

    Second homework task 13

---
+++ Task_13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = n;
+if (n > 99)
+{
+while (a > 999)
+{
+    a = a / 10;
+}
+    int a1 = a % 10;
+    Console.Write(a1);
+}
+else Console.Write("Третьей цифры нет ");
