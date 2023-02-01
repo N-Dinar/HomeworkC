12a9db44e2527707e70241001705e8ffccd7af2c
Dinar <antaresdin@yandex.ru>
2023-02-01 10:59:55 +0500

    Second homework task 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n>999 || n<100)
+Console.WriteLine("Это не трехзначное число ");
+else
+Console.WriteLine(n / 10 % 10);
