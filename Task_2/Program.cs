3a81d88e963c15abba436ef116137d2b7bb5dd4e
Dinar <antaresdin@yandex.ru>
2023-02-04 20:45:13 +0500

    First homework task 2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,11 +1,13 @@
-﻿Console.Clear();
-
+﻿/*  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+Console.Clear();
 Console.Write("Введите 1-ое число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2-ое число: ");
 int m = Convert.ToInt32(Console.ReadLine());
-if(n>m)
+if(n > m)
 Console.WriteLine(n);
 else
 Console.WriteLine(m);
-    
