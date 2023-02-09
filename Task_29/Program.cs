dab65a8eec2540b87ca780dc11c7a4084ec9e6a7
Dinar <antaresdin@yandex.ru>
2023-02-09 14:27:10 +0500

    Fourth homework task 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Требуется вычислить, сколько раз встречается некоторое число X в массиве A[1..N].
+
+Console.Clear();
+Console.Write("Введите количество элемментов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите массив: ");
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+    array[i] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.Write("Введите число X: ");
+int x = Convert.ToInt32(Console.ReadLine()), b = 0;
+for (int i = 0; i < array.Length; i++)
+    if (array[i] == x)
+    b++;
+Console.WriteLine($"Число {x} в массиве встречается {b} раз");
