b1582bd1a46be6c166b9e0f36ee52afdd195a5cb
Dinar <antaresdin@yandex.ru>
2023-02-10 13:50:10 +0500

    Fifth homework additional task

---
+++ AdditionalTask/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите количество дней: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 32); 
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+int count = 0;
+int b = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0)
+    count++;
+    if(array[i] % 2 == 1)
+    b++;
+}
+if(count > b)
+Console.WriteLine("Васе выходит 4!");
+else
+Console.WriteLine("Васе выходит 3!");
