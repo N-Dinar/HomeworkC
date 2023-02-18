1e173f1a689f3504604db9998be6f1425aeb8fa5
Dinar <antaresdin@yandex.ru>
2023-02-18 09:44:52 +0500

    Seventh homework task 52

---
+++ Task_52/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив: 3 4
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
+
+void InputMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+void PrintMatrix(int[, ] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        sum = sum + matrix[i, j];
+        Console.Write($"{ sum / matrix.GetLength(0)}; ");
+    }
+    Console.ReadLine();
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+Console.WriteLine("Начальная матрица: ");
+InputMatrix(matrix);
+Console.WriteLine($"Количество столбцов в массиве: {matrix.GetLength(1)}");
+Console.Write("Среднее арифметическое каждого столбца: ");
+PrintMatrix(matrix);
